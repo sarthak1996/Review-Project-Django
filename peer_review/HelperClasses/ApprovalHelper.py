410396bd2d2c4e77ae209a0fa340542b7ec0d0b6
sarthak1996 <khandekarsarthak@gmail.com>
2020-05-24 17:11:08 +0530

    Implementation change for ApprovalHelper (modularized)

--- peer_review/HelperClasses/ApprovalHelper.py
+++ peer_review/HelperClasses/ApprovalHelper.py
@@ -1,24 +1,38 @@
 from peer_review.models import Review,Approval
 from peer_review.HelperClasses import StatusCodes
 def approve_review(review):
-	latest_approved_row=review.approval_review_assoc.filter(latest=True).all()
-	if latest_approved_row.count()>1:
+	latest_approval_row=get_latest_approval_row(review)
+	status=StatusCodes.get_approved_status()
+	change_status_approval_row(review,latest_approval_row,status)
+	
+def get_latest_approval_row(review):
+	latest_approval_row=review.approval_review_assoc.filter(latest=True).all()
+	if latest_approval_row.count()>1:
 		print('Multiple rows exists for approval')
 		raise Exception('Multiple latest rows exists for approval')
-	if latest_approved_row.count()<0:
+	if latest_approval_row.count()<0:
 		print('No rows exists for approval')
 		raise Exception('No latest rows exists for approval')
-	if latest_approved_row.count()==1:
-		latest_approved_row=latest_approved_row.first()
-		latest_approved_row.approval_outcome=StatusCodes.get_approved_status()
-		latest_approved_row.save()
-		mark_rest_rows_as_not_latest(review,latest_approved_row)
-		review.approval_outcome=StatusCodes.get_approved_status()
-		review.save()
-	
+	if latest_approval_row.count()==1:
+		return latest_approval_row.first()
+	else:
+		raise Exception('Error while fetching latest approval row.'+str(latest_approval_row.count()))
+
+def change_status_approval_row(review,latest_approval_row,status):
+	latest_approval_row.approval_outcome=status
+	latest_approval_row.save()
+	mark_rest_rows_as_not_latest(review,latest_approval_row)
+	review.approval_outcome=status
+	review.save()
 
 def mark_rest_rows_as_not_latest(review,exclude):
 	all_approval_rows=review.approval_review_assoc.all().exclude(pk=exclude.pk)
 	all_approval_rows.update(latest=False)
 	for apr in all_approval_rows:
 		apr.save()
+
+def reject_review(review):
+	latest_approval_row=get_latest_approval_row(review)
+	status=StatusCodes.get_rejected_status()
+	change_status_approval_row(review,latest_approval_row,status)
+
