dfd09f3c2b40ad3d224286500f93aca288957351
sarthak1996 <khandekarsarthak@gmail.com>
2020-06-02 21:53:12 +0530

    Filter on team grp mail to be hidden at first Added multiple text contains filter drop down Add button orientation changes

--- configurations/FilterSets/TeamFilter.py
+++ configurations/FilterSets/TeamFilter.py
@@ -4,6 +4,7 @@ from django import forms
 
 class TeamFilter(django_filters.FilterSet):
 	# Team_text__icontains=django_filters.CharFilter(widget=forms.TextInput(attrs={'placeholder': 'Team text contains'}))
+	team_grp_mail__icontains=django_filters.CharFilter(field_name='team_grp_mail',widget=forms.TextInput(attrs={'class': 'filter_hidden'}),lookup_expr='icontains')
 	class Meta:
 		model=Team
 		fields={'team_name':['icontains'],
--- configurations/ListViews/TeamListView.py
+++ configurations/ListViews/TeamListView.py
@@ -28,12 +28,13 @@ class TeamListView(ListView):
 		print(filter_badge_dict)
 		filter_badges_list=SearchFilterBadges.generate_filter_badges_list(**filter_badge_dict)
 		context['filter_badges']=filter_badges_list
-		context['text_filters_drop_down_icon']=''
+		context['text_filters_drop_down_icon']='dropdown-toggle'
 
 		context['filter']=TeamFilter(self.request.GET,queryset=self.get_queryset(),prefix='filter_form')
 		
 		context['initial_filter']='Team name'
-		context['other_filters']=None
+		context['other_filters']={'filter_form-team_name__icontains':'Team name contains',
+									'filter_form-team_grp_mail__icontains':'Team grp mail contains'}.items()
 
 		context['search_drop_downs']=None
 		context['reset_filters']='configurations:team_list_view'
